febf5c6a03430551dbd87a68c646f7364c2f2a4c
Aditya Ladgotra <ishaladgotra3@gmail.com>
2025-04-21 03:56:14 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 
-from app import db
+from extensions import db
+
 from flask_login import UserMixin
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
